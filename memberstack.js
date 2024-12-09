aaaca2120f397225d58659a7db9429bd4a153472
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-09 09:16:26 +0100

    a

---
+++ memberstack.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", async function () {
+  // shortens reference to Memberstack
+  const memberstack = window.$memberstackDom;
+
+  memberstack.getCurrentMember().then(async ({ data: member }) => {
+    // if the member is logged in
+    if (member) {
+      // Get current member's JSON
+      let memberJson = await memberstack.getMemberJSON();
+
+      // Modify or add new data
+      memberJson.avatarURL = "avatar.jpg";
+
+      // Remove data
+      delete memberJson.skills;
+
+      // Update member's JSON
+      await memberstack.updateMemberJSON({ json: memberJson });
+
+      // Log the updated JSON data
+      console.log(await memberstack.getMemberJSON());
+    }
+  });
+});
