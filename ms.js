df32ab73032cccee958d28c657a7c6c54782b9b1
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-09 10:45:43 +0100

    a

--- ms.js
+++ ms.js
@@ -1,26 +1,66 @@
 document.addEventListener("DOMContentLoaded", async function () {
   const memberstack = window.$memberstackDom;
 
-  memberstack.getCurrentMember().then(async ({ data: member }) => {
-    console.log({ member });
-    if (member) {
-      const memberJson = await memberstack.getMemberJSON(member.id);
-      console.log(memberJson);
+  const displayData = async () => {
+    memberstack.getCurrentMember().then(async ({ data: member }) => {
+      console.log({ member });
+      if (member) {
+        const memberJson = await memberstack.getMemberJSON(member.id);
+        console.log(memberJson);
 
-      const dataElements = document.querySelectorAll("[ms-data-json]");
-      dataElements.forEach((element) => {
-        const key = element.getAttribute("ms-data-json");
-        element.textContent = memberJson.data[key];
-      });
+        const dataElements = document.querySelectorAll("[ms-data-json]");
 
-      // const updatedData = {
-      //   ...memberJson.data,
-      //   test: "test.jpg",
-      // };
+        dataElements.forEach((element) => {
+          const key = element.getAttribute("ms-data-json");
+          element.textContent = memberJson.data[key];
+        });
+      }
+    });
+  };
+  displayData();
 
-      // console.log(updatedData);
+  const addListener = () => {
+    //custom attribute for one button: ms="button-back"
 
-      // await memberstack.updateMemberJSON({ json: updatedData });
-    }
-  });
+    const singleButton = document.getElementById("ms-single-btn");
+    const buttonBack = document.getElementById("ms-back-btn");
+    const yesButton = document.getElementById("ms-yes-btn");
+
+    singleButton.addEventListener("click", async () => {
+      const memberJson = await memberstack.getMemberJSON();
+
+      const updatedData = {
+        ...memberJson.data,
+        maritalStatus: "single",
+        currentStep: memberJson.data.currentStep + 1,
+      };
+
+      await memberstack.updateMemberJSON({ json: updatedData });
+    });
+
+    buttonBack.addEventListener("click", async () => {
+      const memberJson = await memberstack.getMemberJSON();
+
+      const updatedData = {
+        ...memberJson.data,
+        currentStep: memberJson.data.currentStep - 1,
+      };
+
+      await memberstack.updateMemberJSON({ json: updatedData });
+    });
+
+    yesButton.addEventListener("click", async () => {
+      const memberJson = await memberstack.getMemberJSON();
+
+      const updatedData = {
+        ...memberJson.data,
+        hasChildren: true,
+        currentStep: memberJson.data.currentStep + 1,
+      };
+
+      await memberstack.updateMemberJSON({ json: updatedData });
+    });
+  };
+
+  addListener();
 });
