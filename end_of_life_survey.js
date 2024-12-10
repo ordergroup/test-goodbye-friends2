a8dda315864c7abd5ca75759fb85c37f5b6fef49
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-10 08:41:59 +0100

    s

--- end_of_life_survey.js
+++ end_of_life_survey.js
@@ -1,13 +1,12 @@
 console.log("enf of life survey");
 document.addEventListener("DOMContentLoaded", async function () {
   const memberstack = window.$memberstackDom;
-  console.log("here");
 
   const addListener = async () => {
     const form = document.getElementById("End-of-life-survey-1-SK");
 
     const backButton = document.querySelector('[data-form="back-btn"]');
-
+    console.log(backButton);
     backButton.addEventListener("click", async () => {
       const memberJson = await memberstack.getMemberJSON();
       console.log("BACK BUTTON CLICKED");
@@ -21,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     //get all inputs in form and listen for changes
     const inputs = form.querySelectorAll("input");
-    console.log(inputs);
+    // console.log(inputs);
     inputs.forEach((input) => {
       input.addEventListener("change", async () => {
         const memberJson = await memberstack.getMemberJSON();
