d335cff73b337efd4169ee898f2fbb5dd7456286
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-10 08:39:30 +0100

    s

--- end_of_life_survey.js
+++ end_of_life_survey.js
@@ -6,23 +6,34 @@ document.addEventListener("DOMContentLoaded", async function () {
   const addListener = async () => {
     const form = document.getElementById("End-of-life-survey-1-SK");
 
-    const memberJson = await memberstack.getMemberJSON();
+    const backButton = document.querySelector('[data-form="back-btn"]');
 
-    console.log(form);
+    backButton.addEventListener("click", async () => {
+      const memberJson = await memberstack.getMemberJSON();
+      console.log("BACK BUTTON CLICKED");
+      const updatedData = {
+        ...memberJson.data,
+        currentStep: memberJson.data.currentStep - 1,
+      };
+
+      await memberstack.updateMemberJSON({ json: updatedData });
+    });
 
     //get all inputs in form and listen for changes
     const inputs = form.querySelectorAll("input");
     console.log(inputs);
     inputs.forEach((input) => {
       input.addEventListener("change", async () => {
-        // const memberJson = await memberstack.getMemberJSON();
+        const memberJson = await memberstack.getMemberJSON();
         console.log(input.name, input.value);
-        // const updatedData = {
-        //   ...memberJson.data,
-        //   [input.name]: input.value,
-        // };
 
-        // await memberstack.updateMemberJSON({ json: updatedData });
+        const updatedData = {
+          ...memberJson.data,
+          [input.name]: input.value,
+          currentStep: memberJson.data.currentStep + 1,
+        };
+
+        await memberstack.updateMemberJSON({ json: updatedData });
       });
     });
   };
