2ecf49894ba23e54ccf7da331430f0a1a91e53af
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-10 08:17:51 +0100

    s

---
+++ end_of_life_survey.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", async function () {
+  const memberstack = window.$memberstackDom;
+
+  const addListener = async () => {
+    const form = document.getElementById("End-of-life-survey-1-SK");
+
+    const memberJson = await memberstack.getMemberJSON();
+
+    console.log(form);
+
+    //get all inputs in form and listen for changes
+    const inputs = form.querySelectorAll("input");
+    console.log(inputs);
+    inputs.forEach((input) => {
+      input.addEventListener("change", async () => {
+        // const memberJson = await memberstack.getMemberJSON();
+        console.log(input.name, input.value);
+        // const updatedData = {
+        //   ...memberJson.data,
+        //   [input.name]: input.value,
+        // };
+
+        // await memberstack.updateMemberJSON({ json: updatedData });
+      });
+    });
+  };
+
+  addListener();
+});
