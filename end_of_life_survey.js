37794f463fe18dafef7752eb95c80c6839cbeb40
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-12-12 13:35:40 +0100

    s

--- end_of_life_survey.js
+++ end_of_life_survey.js
@@ -25,12 +25,10 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     const select = form.querySelectorAll("select");
     select.forEach((select) => {
-      console.log("here");
       if (existingData[select.name]) select.value = existingData[select.name];
     });
   };
 
-  // Get initial member data and populate localStorage
   const initializeLocalStorage = async () => {
     const memberJson = (await memberstack.getMemberJSON()) || {};
     localStorage.setItem("surveyData", JSON.stringify(memberJson.data));
@@ -40,7 +38,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     displaySelectedData();
   };
 
-  // Save data to localStorage
   const saveToLocalStorage = (key, value) => {
     const existingData = JSON.parse(localStorage.getItem("surveyData")) || {};
     const updatedData = { ...existingData, [key]: value };
@@ -50,38 +47,26 @@ document.addEventListener("DOMContentLoaded", async function () {
     displaySelectedData();
   };
 
-  // Send data to Memberstack if data has changed
   const sendDataToMemberstack = async () => {
     const storedData = JSON.parse(localStorage.getItem("surveyData"));
     if (JSON.stringify(storedData) !== lastSyncedData) {
       console.log("Syncing Data to Memberstack:", storedData);
-
       if (storedData) {
         await memberstack.updateMemberJSON({ json: storedData });
-        console.log("Data sent to Memberstack:", storedData);
-        lastSyncedData = JSON.stringify(storedData); // Update last synced state
+        lastSyncedData = JSON.stringify(storedData);
       }
     } else {
       console.log("No changes in data; skipping sync.");
     }
   };
 
-  // Periodically check and send data to Memberstack if it has changed
   const startDataSync = () => {
     setInterval(async () => {
       await sendDataToMemberstack();
-    }, 5000); // Sync every 5 seconds
+    }, 5000);
   };
 
-  // Send data immediately when the user leaves the page
-  const handleBeforeUnload = () => {
-    sendDataToMemberstack();
-    console.log("Data sent before page unload");
-  };
-
-  // Add listeners to inputs and back buttons
   const addListeners = () => {
-    // Handle back button clicks
     const backButtons = form.querySelectorAll('[data-form="back-btn"]');
     backButtons.forEach((button) => {
       button.addEventListener("click", () => {
@@ -109,9 +94,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
   };
 
-  // Initialize, start syncing, and handle unload
   await initializeLocalStorage();
   addListeners();
   startDataSync();
-  window.addEventListener("beforeunload", handleBeforeUnload);
 });
